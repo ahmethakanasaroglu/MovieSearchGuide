5c627a949e949033cb1fa60770ba8de1f93e425c
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-01-31 14:49:46 +0300

    Initial Commit

---
+++ FilmAramaSwiftUI/FilmAramaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAramaSwiftUIApp.swift
+//  FilmAramaSwiftUI
+//
+//  Created by Ahmet Hakan Asaroğlu on 31.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FilmAramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
